cb06f35553317666d751e403215956302a29a2f1
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-07-19 23:17:37 -0300

    css-add-login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,55 @@
 'use client'
+import Button from '@/atoms/button/Button'
 import Input from '@/atoms/input/Input'
+import { login } from '@/utils/images'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
     <div className="login">
-      <Input
-        handleChange={() => {}}
-        label="Nombre de usuario"
-        name="name"
-        place="Usuario"
-        value=""
-        iconLeft="user"
-        input
-      />
-      <Input
-        handleChange={() => {}}
-        label="Contraseña"
-        name="categoryName"
-        place="Contraseña"
-        value=""
-        iconLeft="padlock"
-        iconRigth="visibility"
-        input
-      />
+      <div className="login__landing">
+        <h1 className="login__h3">
+          Regaleria
+          <span className="login__span"> Milagros</span>
+        </h1>
+        <Image
+          src={login}
+          alt="Imagen de login"
+          width={500}
+          height={500}
+          className="login__image"
+        />
+      </div>
+      <div className="login__container">
+        <form className="login__form">
+          <Input
+            handleChange={() => {}}
+            label="Nombre de usuario"
+            name="name"
+            place="Usuario"
+            value=""
+            iconLeft="user"
+            input
+          />
+          <Input
+            handleChange={() => {}}
+            label="Contraseña"
+            name="categoryName"
+            place="Contraseña"
+            value=""
+            iconLeft="padlock"
+            iconRigth="visibility"
+            input
+          />
+          <Button
+            click={() => {}}
+            classN="button__yellow"
+            text="Iniciar Sesion"
+            classNIcon="button__color"
+          />
+        </form>
+      </div>
     </div>
   )
 }
---
+++ src/utils/images.ts
@@ -0,0 +1,2 @@
+export const login =
+  'https://res.cloudinary.com/damjxqb5f/image/upload/v1689818756/4048544_aqdbso.jpg'
